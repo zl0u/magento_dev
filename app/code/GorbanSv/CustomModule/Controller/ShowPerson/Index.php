b01605d2d851a8ad196a20200ddc0d46b131807b
Sergey Gorban <serg.ck.ua@gmail.com>
2018-10-30 01:01:34 +0200

    Lesson 3: Create custom Controller ShowPerson, add Layout with CustomBlock and template

---
+++ app/code/GorbanSv/CustomModule/Controller/ShowPerson/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GorbanSv\CustomModule\Controller\ShowPerson;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}
