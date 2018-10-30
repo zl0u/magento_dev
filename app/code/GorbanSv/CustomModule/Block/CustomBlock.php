6f570b41bb21b0fc143621d59abf8ca3e72379f4
Sergey Gorban <serg.ck.ua@gmail.com>
2018-10-30 02:43:38 +0200

    Lesson 3: Add custom block CustomBlock

---
+++ app/code/GorbanSv/CustomModule/Block/CustomBlock.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GorbanSv\CustomModule\Block;
+
+class CustomBlock extends \Magento\Framework\View\Element\Template
+{
+    const CUSTOM_BLOCK_TEMPLATE = "GorbanSv_CustomModule::custom_module/myhomework.phtml";
+
+    /**
+     * add custom template
+     */
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        $this->setTemplate(self::CUSTOM_BLOCK_TEMPLATE);
+
+        return $this;
+    }
+}
