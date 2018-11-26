62f52eb296010cd1e974090d8d58d1aee9068a02
Sergey Gorban <serg.ck.ua@gmail.com>
2018-11-27 01:05:05 +0200

    Lesson 6: Do not allow to send questions more than once per 2 minutes

--- app/code/GorbanSv/AskQuestion/view/frontend/web/js/ask-question.js
+++ app/code/GorbanSv/AskQuestion/view/frontend/web/js/ask-question.js
@@ -10,6 +10,9 @@ define([
 
     $.widget('gorbansv.requestSample', {
         options: {
+            cookieName: 'gorbansv_ask_question',
+            cookieLifetime: 2,
+            timeZoneOffset: 1
         },
 
         /** @inheritdoc */
@@ -34,7 +37,12 @@ define([
          * Submit request via AJAX. Add form key to the post data.
          */
         ajaxSubmit: function () {
-            var formData = new FormData($(this.element).get(0));
+            var formData = new FormData($(this.element).get(0)),
+                d = new Date(),
+                utc = d.getTime() + (d.getTimezoneOffset() * 60000),
+                nd = new Date(
+                    utc + (3600000 * this.options.timeZoneOffset) + (this.options.cookieLifetime * 60 * 1000)
+                );
 
             formData.append('form_key', $.mage.cookies.get('form_key'));
             formData.append('isAjax', 1);
@@ -50,6 +58,14 @@ define([
 
                 /** @inheritdoc */
                 beforeSend: function () {
+                    if ($.mage.cookies.get(this.options.cookieName)) {
+                        alert({
+                            title: $.mage.__('Error'),
+                            content: $.mage.__('It is forbidden to send questions more than once per 2 minutes.')
+                        });
+
+                        return false;
+                    }
                     $('body').trigger('processStart');
                 },
 
@@ -60,6 +76,12 @@ define([
                         title: $.mage.__(response.status),
                         content: $.mage.__(response.message)
                     });
+
+                    if (response.status === 'Success') {
+                        $.mage.cookies.set(this.options.cookieName, true, {
+                            expires: nd
+                        });
+                    }
                 },
 
                 /** @inheritdoc */
