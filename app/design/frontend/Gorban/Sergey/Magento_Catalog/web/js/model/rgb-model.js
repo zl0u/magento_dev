29a4f564a0edbfdc5c170567114d1c7746ee591f
Sergey Gorban <serg.ck.ua@gmail.com>
2018-12-02 15:15:14 +0200

    Lesson 7: Catalog Timer

---
+++ app/design/frontend/Gorban/Sergey/Magento_Catalog/web/js/model/rgb-model.js
@@ -0,0 +1,24 @@
+define(
+    ['ko'],
+    function (ko) {
+        'use strict';
+        var red = ko.observable(0);
+        var blue = ko.observable(0);
+        var green = ko.observable(0);
+        function randomNumber() {
+            return Math.floor((Math.random() * 255) + 1);
+        }
+        function updateColour() {
+            red(randomNumber());
+            blue(randomNumber());
+            green(randomNumber());
+        }
+        return {
+            randomNumber: randomNumber,
+            updateColour: updateColour,
+            red: red,
+            blue: blue,
+            green: green
+        };
+    }
+);
